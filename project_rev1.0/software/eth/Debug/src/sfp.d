de53959f941b5784d062cc1fd892ccec52f9119e
CagriMeseli <167322751+CagriMeseli@users.noreply.github.com>
2024-05-07 01:44:51 +0300

    Add files via upload

---
+++ project_rev1.0/software/eth/Debug/src/sfp.d
@@ -0,0 +1,7 @@
+src/sfp.o src/sfp.o: ../src/sfp.c \
+ ../../eth_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../eth_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../../eth_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../eth_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
